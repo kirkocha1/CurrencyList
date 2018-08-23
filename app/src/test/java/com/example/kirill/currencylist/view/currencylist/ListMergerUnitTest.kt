d492e4aaf0ba0aa53534be11873359043db6f3ac
Kirill Kochnev <kirkocha@gmail.com>
2018-08-23 12:42:31 +0300

    add additional test

--- app/src/test/java/com/example/kirill/currencylist/view/currencylist/ListMergerUnitTest.kt
+++ app/src/test/java/com/example/kirill/currencylist/view/currencylist/ListMergerUnitTest.kt
@@ -20,7 +20,17 @@ class ListMergerUnitTest {
         }
     }
 
-
+    @Test
+    fun check_result_list_contains_only_repeated_and_new_elements_from_map() {
+        val merger = ListMerger()
+        val testPrevList = constructList()
+        val newTestDataMap = constructMap()
+        val result = merger.constructUpdatedList(testPrevList, newTestDataMap)
+        for (i in 0..result.size) {
+            assert(result[i].currencyCode != "FAKE")
+        }
+    }
+    
     private fun constructList() = mutableListOf<CurrencyItemUnit>().apply {
         add(CurrencyItemUnit("EUR", 1.toBigDecimal()))
         add(CurrencyItemUnit("BGN", 1.toBigDecimal()))
@@ -28,6 +38,16 @@ class ListMergerUnitTest {
         add(CurrencyItemUnit("CAD", 1.toBigDecimal()))
         add(CurrencyItemUnit("CHF", 1.toBigDecimal()))
         add(CurrencyItemUnit("CNY", 1.toBigDecimal()))
+    }
+
+    private fun constructAnotherList() = mutableListOf<CurrencyItemUnit>().apply {
+        add(CurrencyItemUnit("EUR", 1.toBigDecimal()))
+        add(CurrencyItemUnit("BGN", 1.toBigDecimal()))
+        add(CurrencyItemUnit("BRL", 1.toBigDecimal()))
+        add(CurrencyItemUnit("CAD", 1.toBigDecimal()))
+        add(CurrencyItemUnit("CHF", 1.toBigDecimal()))
+        add(CurrencyItemUnit("CNY", 1.toBigDecimal()))
+        add(CurrencyItemUnit("FAKE", 1.toBigDecimal()))
     }
 
     private fun constructMap() = mutableMapOf<String, BigDecimal>().apply {
